67b4e2df436c3ed37260ddbd52b1727a884d657a
DiceRollerK <you@example.com>
2026-09-15 15:08:15 +0300

    Pievienota iespēja izmantot space like apgriezt mašīnu spoguļattēlā

--- Assets/Scripts/ObjectTransformationScript.cs
+++ Assets/Scripts/ObjectTransformationScript.cs
@@ -55,6 +55,11 @@ public class ObjectTransformationScript : MonoBehaviour
                     GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale = new Vector3(GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x + 0.002f, GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y, 1f);
                 }
             }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale = new Vector3(GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x * -1, GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y, 1f);
+            }
         }
     }
 }
